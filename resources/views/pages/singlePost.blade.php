660da3514f26d0f07851dd813dde5dd9fa9625cb
Joana Jacome <jisjacome@gmail.com>
2018-07-13 10:27:35 +0200

    final changes

--- resources/views/pages/singlePost.blade.php
+++ resources/views/pages/singlePost.blade.php
@@ -1,5 +1,5 @@
 
-@extends('layouts.main')
+@extends('layouts.master')
 @section('content')
     <!-- Page Header -->
     <header class="masthead" style="background-image: url('{{asset('assets/img/post-bg.jpg')}}')">
